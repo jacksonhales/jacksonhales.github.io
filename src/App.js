fafe25c6b7221ea8282ef443dd1bf2c1ecb8572e
Jackson Hales <jacksonjhales@gmail.com>
2026-01-05 20:55:31 +1100

    Remove routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import Navbar from "./components/Global/Navbar";
+// import Navbar from "./components/Global/Navbar";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Pages/Home";
-import Projects from "./components/Pages/Projects";
-import Photography from "./components/Pages/Photography";
+// import Projects from "./components/Pages/Projects";
+// import Photography from "./components/Pages/Photography";
 import Footer from "./components/Global/Footer";
-import Art from "./components/Pages/Art";
+// import Art from "./components/Pages/Art";
 
 function App() {
   return (
     <>
       <HashRouter basename="/">
         <div className="min-h-screen w-full p-4 bg-base-300">
-          <Navbar />
+          {/* <Navbar /> */}
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/photography" element={<Photography />} />
-            <Route path="/art" element={<Art />} />
+            {/* <Route path="/projects" element={<Projects />} /> */}
+            {/* <Route path="/photography" element={<Photography />} /> */}
+            {/* <Route path="/art" element={<Art />} /> */}
           </Routes>
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </HashRouter>
     </>
